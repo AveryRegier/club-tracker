4b630f0835d5ba82db38b12b98360d6904a30193
rx39789 <RegierAveryJ@JohnDeere.com>
2014-09-10 20:00:07 -0500

    oops

--- club-web/src/main/java/com/github/averyregier/club/domain/program/adapter/Later.java
+++ club-web/src/main/java/com/github/averyregier/club/domain/program/adapter/Later.java
@@ -6,11 +6,11 @@ package com.github.averyregier.club.domain.program.adapter;
 public class Later<T> {
     private T value;
     public T get() {
-        assert(this.value == null);
+        assert(this.value != null);
         return value;
     }
     public void set(T value) {
-        assert(this.value != null);
+        assert(this.value == null);
         this.value = value;
     }
 }
