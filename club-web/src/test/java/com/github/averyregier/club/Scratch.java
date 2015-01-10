5cdf9ec7ae2d3a1c38c25b571b7686b8687265d0
Avery Regier <avery.regier@gmail.com>
2015-01-09 22:35:17 -0600

    Milestone: the original scratch program works!

--- club-web/src/test/java/com/github/averyregier/club/Scratch.java
+++ club-web/src/test/java/com/github/averyregier/club/Scratch.java
@@ -1,8 +1,12 @@
 package com.github.averyregier.club;
 
-import com.github.averyregier.club.domain.PersonManager;
+import com.github.averyregier.club.domain.User;
+import com.github.averyregier.club.domain.UserManager;
 import com.github.averyregier.club.domain.club.*;
+import com.github.averyregier.club.domain.club.adapter.ProgramAdapter;
 import com.github.averyregier.club.domain.program.Section;
+import com.github.averyregier.club.domain.utility.UtilityMethods;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -13,14 +17,47 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static com.github.averyregier.club.TestUtility.anyEqual;
+
 /**
  * Created by avery on 9/6/2014.
  */
 public class Scratch {
+    private UserManager userManager;
+    private String personId;
+
+    @Before
+    public void setup() {
+        userManager = new UserManager();
+        User diane = userManager.createUser("Diane");
+        diane.getUpdater().setGender(Person.Gender.FEMALE);
+
+        ProgramAdapter program = new ProgramAdapter("en_US", null, "AWANA");
+        program.setPersonManager(userManager.getPersonManager());
+        Club club = program.addClub(program.getCurriculum().getSeries("TnT").get());
+
+        club.recruit(diane);
+
+        User parent = userManager.createUser("Parent");
+        Map<String, String> values = UtilityMethods.map("child1.childName.friendly", "Betty")
+                .put("child1.ageGroup", "THIRD_GRADE")
+                .put("child1.gender", "FEMALE")
+                .build();
+        Family family = program.updateRegistrationForm(values).register(parent);
+        Clubber clubber = family.getClubbers().stream().findFirst().get();
+
+        club.getCurriculum()
+                .recommendedBookList(clubber.getCurrentAgeGroup()).stream()
+                .flatMap(b -> b.getSections().stream())
+                .filter(s -> anyEqual(s.getGroup().sequence(), 0, 1, 2))
+                .forEach(s -> clubber.getRecord(Optional.of(s)).ifPresent(r -> r.sign(diane.asListener().get(), "")));
+
+        personId = clubber.getId();
+    }
+
     @Test
     public void checkoffList() {
-        PersonManager personManager = new PersonManager();
-        Optional<Person> user = personManager.lookup("Diane");
+        Optional<User> user = userManager.getUser("Diane");
         if(user.isPresent() && user.get().asListener().isPresent()) {
             Listener me = user.get().asListener().get();
             {
@@ -55,10 +92,9 @@ public class Scratch {
             {
                 // in code, we'd be in a new transaction, trying to put together the action from the identities
                 // POST /clubbers/{personID}/sections/{sectionID}/sign
-                String personId = "1234-5678";
-                String sectionId = "A-TnT-3b-3-1";
+                String sectionId = "TnT:UA:1©2010:3:2";
                 String note = "Word Perfect!";
-                personManager.lookup(personId)
+                userManager.getPersonManager().lookup(personId)
                         .ifPresent(p -> p.asClubber()
                                 .ifPresent(c -> c.getRecord(c.getClub().get().getCurriculum().lookup(sectionId))
                                         .ifPresent(r -> {
