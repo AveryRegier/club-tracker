035cb54b8c792f1cdb119ecf999af2a9b700575e
Avery Regier <avery.regier@gmail.com>
2015-01-10 10:21:09 -0600

    fast setup of a very small club for testing

--- club-web/src/main/java/com/github/averyregier/club/view/FastSetup.java
+++ club-web/src/main/java/com/github/averyregier/club/view/FastSetup.java
@@ -2,7 +2,15 @@ package com.github.averyregier.club.view;
 
 import com.github.averyregier.club.application.ClubApplication;
 import com.github.averyregier.club.domain.User;
+import com.github.averyregier.club.domain.club.ClubLeader;
+import com.github.averyregier.club.domain.club.Family;
+import com.github.averyregier.club.domain.club.Program;
+import com.github.averyregier.club.domain.club.RegistrationInformation;
 import org.brickred.socialauth.Profile;
+import spark.Request;
+import spark.Response;
+
+import java.util.Map;
 
 import static spark.Spark.before;
 import static spark.Spark.halt;
@@ -13,19 +21,72 @@ import static spark.Spark.halt;
 public class FastSetup {
     public void init(ClubApplication app) {
         before("/test-setup", (request, response) -> {
-            String context = request.contextPath();
-            context = context == null ? "" : context;
-            String location = context + "/protected/my";
-            Profile profile = new Profile();
-            profile.setFirstName("John");
-            profile.setLastName("Doe");
-            profile.setGender("male");
-            profile.setEmail("john.doe@example.com");
-            User user = Login.setupUser(app, profile);
-            request.session().attribute("location", location);
-            Login.resetCookies(request, response, user.getId(), user);
+            User user = setupJohnDoe(app);
+            synchronized (this) {
+                if (app.getProgram() == null) {
+
+                    Program program = app.setupProgram("ABC", "AWANA", "en_US");
+                    program.getCurriculum().getSeries().stream().forEach(program::addClub);
+
+                    program.assign(user, ClubLeader.LeadershipRole.COMMANDER);
+                    program.getClubs().stream().findFirst().ifPresent(c -> c.recruit(user));
+
+                    registerDoeFamily(user, program);
+
+                    registerSmithFamily(app);
+                }
+            }
+            goToMy(request, response, user);
             halt();
         });
 
+    }
+
+    private void registerSmithFamily(ClubApplication app) {
+        Profile profile = new Profile();
+        profile.setFirstName("Mary");
+        profile.setLastName("Smith");
+        profile.setGender("female");
+        profile.setEmail("mary.smith@example.com");
+        profile.setValidatedId("Example-ID-For-Mary-Smith");
+        User user = Login.setupUser(app, profile);
+
+        Program program = app.getProgram();
+
+        RegistrationInformation form = program.createRegistrationForm(user);
+        Map<String, String> fields = form.getFields();
+        fields.put("child1.childName.given", "Joe");
+        fields.put("child1.childName.surname", "Smith");
+        fields.put("child1.gender", "MALE");
+        fields.put("child1.ageGroup", "THIRD_GRADE");
+        Family family = program.updateRegistrationForm(fields).register(user);
+    }
+
+    private void registerDoeFamily(User user, Program program) {
+        RegistrationInformation form = program.createRegistrationForm(user);
+        Map<String, String> fields = form.getFields();
+        fields.put("child1.childName.given", "Betty");
+        fields.put("child1.childName.surname", "Doe");
+        fields.put("child1.gender", "FEMALE");
+        fields.put("child1.ageGroup", "FOURTH_GRADE");
+        Family family = program.updateRegistrationForm(fields).register(user);
+    }
+
+    private void goToMy(Request request, Response response, User user) {
+        String context = request.contextPath();
+        context = context == null ? "" : context;
+        String location = context + "/protected/my";
+        request.session().attribute("location", location);
+        Login.resetCookies(request, response, user.getId(), user);
+    }
+
+    private User setupJohnDoe(ClubApplication app) {
+        Profile profile = new Profile();
+        profile.setFirstName("John");
+        profile.setLastName("Doe");
+        profile.setGender("male");
+        profile.setEmail("john.doe@example.com");
+        profile.setValidatedId("Example-ID-For-John-Doe");
+        return Login.setupUser(app, profile);
     }
 }
