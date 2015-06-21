f968378505605a9b0efd9a11f3a7098713665fb6
Avery Regier <avery.regier@gmail.com>
2015-06-20 21:38:12 -0500

    load all login providers from the database

--- club-web/src/main/java/com/github/averyregier/club/broker/ProviderBroker.java
+++ club-web/src/main/java/com/github/averyregier/club/broker/ProviderBroker.java
@@ -4,8 +4,11 @@ import com.github.averyregier.club.db.tables.records.ProviderRecord;
 import com.github.averyregier.club.domain.login.Provider;
 import org.jooq.DSLContext;
 import org.jooq.Field;
+import org.jooq.Result;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.github.averyregier.club.db.tables.Provider.PROVIDER;
 
@@ -38,5 +41,14 @@ public class ProviderBroker extends Broker<Provider> {
                 .set(PROVIDER.CLIENT_KEY, thing.getClientKey())
                 .set(PROVIDER.SECRET, thing.getSecret())
                 .build();
+    }
+
+    public List<Provider> find() {
+        return query(create->{
+            Result<ProviderRecord> records = create.selectFrom(PROVIDER).fetch();
+            return records.stream().map(r-> new Provider(
+                    r.getProviderId(), r.getName(), r.getImage(), r.getSite(), r.getClientKey(), r.getSecret()
+            )).collect(Collectors.toList());
+        });
     }
 }
--- club-web/src/test/java/com/github/averyregier/club/broker/ProviderBrokerTest.java
+++ club-web/src/test/java/com/github/averyregier/club/broker/ProviderBrokerTest.java
@@ -1,15 +1,22 @@
 package com.github.averyregier.club.broker;
 
+import com.github.averyregier.club.db.tables.records.ProviderRecord;
 import com.github.averyregier.club.domain.login.Provider;
+import org.jooq.Result;
 import org.jooq.exception.DataAccessException;
 import org.jooq.tools.jdbc.MockDataProvider;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 import static com.github.averyregier.club.broker.BrokerTestUtil.mergeProvider;
 import static com.github.averyregier.club.broker.BrokerTestUtil.mockConnector;
+import static com.github.averyregier.club.broker.BrokerTestUtil.select;
 import static com.github.averyregier.club.db.tables.Provider.PROVIDER;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ProviderBrokerTest {
 
@@ -59,5 +66,49 @@ public class ProviderBrokerTest {
 
     private ProviderBroker setup(MockDataProvider provider) {
         return new ProviderBroker(mockConnector(provider));
+    }
+
+    @Test
+    public void findNoProviders() {
+        MockDataProvider provider = select((s) -> {
+            // TODO: find a way to assert empty where clause
+        }, (r) -> r.newResult(PROVIDER));
+
+        assertTrue(setup(provider).find().isEmpty());
+    }
+
+    @Test
+    public void findAllProviders() {
+        List<ProviderRecord> allRecords = Arrays.asList(new ProviderRecord(
+                "providerId",
+                "site",
+                "name",
+                "image",
+                "clientKey",
+                "secret"
+        ));
+
+        MockDataProvider provider = select((s) -> {
+            // TODO: find a way to assert empty where clause
+        }, (r) -> {
+            Result<ProviderRecord> result = r.newResult(PROVIDER);
+            result.addAll(allRecords);
+            return result;
+        });
+
+        List<Provider> providers = setup(provider).find();
+
+        assertEquals(1, providers.size());
+
+        int i=0;
+        for(Provider p: providers) {
+            ProviderRecord record = allRecords.get(i++);
+            assertEquals(record.getClientKey(), p.getClientKey());
+            assertEquals(record.getImage(), p.getImage());
+            assertEquals(record.getName(), p.getName());
+            assertEquals(record.getProviderId(), p.getId());
+            assertEquals(record.getSite(), p.getSite());
+            assertEquals(record.getSecret(), p.getSecret());
+        }
     }
 }
