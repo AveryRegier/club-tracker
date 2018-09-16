1f3ae808a0e3b1e3046b5dcce32da212b23acd69
Avery Regier <avery.regier@gmail.com>
2018-09-16 14:31:34 -0500

    refactor

--- club-web/src/main/java/com/github/averyregier/club/view/InitialSetup.java
+++ club-web/src/main/java/com/github/averyregier/club/view/InitialSetup.java
@@ -13,33 +13,32 @@ import static spark.Spark.*;
  */
 public class InitialSetup extends BaseController {
     public void init(ClubApplication app) {
-        before("/initial-setup", (request, response) -> {
-            List<Provider> providers = new ProviderBroker(app.getConnector()).find();
-            if(providers.isEmpty()) {
+        path("/initial-setup", ()->{
+            before("", (request, response) -> {
+                List<Provider> providers = new ProviderBroker(app.getConnector()).find();
+                if (!providers.isEmpty()) {
+                    halt();
+                }
+            });
 
-            } else {
-                halt();
-            }
-        });
+            get("", (request, response)-> render(
+                    newModel(request, "Add Login Provider").build(),
+                    "provider.ftl"));
 
-        get("/initial-setup", (request, response)-> render(
-                newModel(request, "Add Login Provider").build(),
-                "provider.ftl"));
+            post("", (request, response) -> {
+                String id = request.queryParams("providerId");
+                String providerName = request.queryParams("providerName");
+                String site = request.queryParams("site");
+                String image = request.queryParams("image");
+                String clientKey = request.queryParams("clientKey");
+                String clientSecret = request.queryParams("clientSecret");
 
-        post("/initial-setup", (request, response) -> {
+                new ProviderBroker(app.getConnector()).persist(new Provider(
+                        id, providerName, image, site, clientKey, clientSecret));
 
-            String id = request.queryParams("providerId");
-            String providerName = request.queryParams("providerName");
-            String site = request.queryParams("site");
-            String image = request.queryParams("image");
-            String clientKey = request.queryParams("clientKey");
-            String clientSecret = request.queryParams("clientSecret");
-
-            new ProviderBroker(app.getConnector()).persist(new Provider(
-                    id, providerName, image, site, clientKey, clientSecret));
-
-            response.redirect("/protected/setup");
-            return null;
+                response.redirect("/protected/setup");
+                return null;
+            });
         });
     }
 }
